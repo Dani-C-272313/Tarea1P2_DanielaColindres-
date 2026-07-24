557937f874a3c93e2a715203f7c54610b1e147cd
Daniela Colindres <dani.colindrez27@gmail.com>
2026-07-23 23:46:03 -0600

    Clase inventario hecha

---
+++ src/tarea1_p2/Inventario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea1_p2;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Inventario {
+    
+}
